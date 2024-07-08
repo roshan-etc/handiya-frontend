cf7e9ae5fbfd6617ea4b7bc084f5e1253203d9a8
Roshan <roshan11feb@gmail.com>
2024-07-08 10:41:47 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayURL: 'ApplicationLB-492902276.us-east-2.elb.amazonaws.com'
+  apiGatewayURL: 'Applicationloadblncer-859045254.us-east-2.elb.amazonaws.com'
 };
